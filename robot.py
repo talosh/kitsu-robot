ae7be947fe2268879b09bde0ed07b36b12d6eb71
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-06-20 13:22:45 +0100

    update robot.py and setenv.py

--- robot.py
+++ robot.py
@@ -16,6 +16,7 @@ DEBUG=True
 __version__ = 'v0.0.1'
 
 if __name__ == "__main__":
+    os.system('/usr/bin/env python /opt/kitsu-robot/setenv.py')
     
     '''
     print ('... starting robot ...')
