b1400f8338ca270a6f31c6cf7dca92b5ce7ef770
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-06-20 13:15:36 +0100

    update init_mac.sh

--- robotenv/bin/init_mac.sh
+++ robotenv/bin/init_mac.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 /usr/bin/osascript -e 'tell application "Terminal" to activate'
-/usr/bin/osascript -e 'tell application "Terminal" to do script "/opt/kitsu-robot/robotenv/bin/python3 /opt/kitsu-robot/setenv.py" in window 1'
-/usr/bin/osascript -e 'tell application "Terminal" to do script "screen -L -c /opt/kitsu-robot/screen.conf -S Kitsu-Robot -d -m /opt/kitsu-robot/robotenv/bin/python3 /opt/kitsu-robot/robot.py" in window 1'
+/usr/bin/osascript -e 'tell application "Terminal" to do script "/opt/kitsu-robot/robotenv/bin/python3 /opt/kitsu-robot/setenv.py; screen -L -c /opt/kitsu-robot/screen.conf -S Kitsu-Robot -d -m /opt/kitsu-robot/robotenv/bin/python3 /opt/kitsu-robot/robot.py" in window 1'
 sleep 2
 /usr/bin/osascript -e 'quit application "Terminal"'
