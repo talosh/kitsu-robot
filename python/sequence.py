91702d1d5b61b415af29c6549041ae5ad673ae8f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-07-22 11:36:54 +0100

    update robot.py

---
+++ python/sequence.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import time
+from pprint import pprint, pformat
+
+def sequence_sync(config):
+    pprint (config)
+    sys.exit()
--- robot.py
+++ robot.py
@@ -13,11 +13,11 @@ from pprint import pprint, pformat
 from python.config import get_config_data
 from python.tailon import tailon
 from python.metadata_fields import set_metadata_fields
-
+from python.sequence import sequence_sync
 
 DEBUG=True
 
-__version__ = 'v0.0.1'
+__version__ = 'v0.0.2'
 
 if __name__ == "__main__":
     app_location = os.path.dirname(os.path.abspath(__file__))
@@ -111,6 +111,9 @@ if __name__ == "__main__":
     metadata_thread.daemon = True
     metadata_thread.start()
 
+    sequence_sync_thread = threading.Thread(target=sequence_sync, args=(app_config, ))
+    sequence_sync_thread.daemon = True
+    sequence_sync_thread.start()
     
     import gazu
     config = app_config
