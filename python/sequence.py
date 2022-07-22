6fd4f74ed945870ce583d55cf859d4fb5e6c2387
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-07-22 13:30:18 +0100

    update sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -7,3 +7,12 @@ def sequence_sync(config):
     log = config.get('log')
     flapi_module_path = config.get('flapi_module_path')
     log.verbose('importing flapi from %s' % flapi_module_path)
+
+    try:
+        if sys.path[0] != flapi_module_path:
+            sys.path.insert(0, flapi_module_path)
+        import flapi
+    except Exception as e:
+        log.error('unable to import filmlight api python module from: %s' % flapi_module_path)
+        log.error(e)
+
