9059910f2fb0862ef3fa330bbfacd622279ce5b2
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-07-29 15:04:35 +0100

    update 127 files

--- python/sequence.py
+++ python/sequence.py
@@ -78,6 +78,18 @@ def get_baselight_scene_shots(config, blpath):
     scene_path = fl_get_scene_path(config, flapi, conn, blpath)
     if not scene_path:
         return []
+
+    try:
+        scene = conn.Scene.open_scene( scene_path, { flapi.OPENFLAG_READ_ONLY } )
+    except flapi.FLAPIException as ex:
+        log.error( "Error loading scene: %s" % ex )
+        return []
+
+    nshots = scene.get_num_shots()
+    print( "Found %d shot(s)" % nshots )
+
+    scene.close_scene()
+    scene.release()
     
     fl_disconnect(config, flapi, flapi_host, conn)
 
