68c26b20dcc089a5940f02c9bf134aa8d1357d0b
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-08-01 12:36:01 +0100

    update sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -118,7 +118,7 @@ def get_baselight_scene_shots(config, blpath):
 
             shot.release()
 
-    test_tc = flapi.Utilities.timecode_from_string('01:00:00:00')
+    test_tc = conn.Utilities.timecode_from_string('01:00:00:00')
     pprint (test_tc)
     pprint (dir(test_tc))
     sys.exit()
