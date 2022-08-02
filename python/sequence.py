046e335a6ba004f806a52475aa557304b7b0593e
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-08-02 15:44:34 +0100

    update metadata_descriptors.json and sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -100,9 +100,9 @@ def build_kitsu_shot_data(config, baselight_shot):
         if not bl_key:
             continue
         md_descriptors_by_bl_key[bl_key] = md_desc
-    pprint (md_descriptors_by_bl_key)
     shot_md = baselight_shot.get('shot_md')
-
+    for bl_key in md_descriptors_by_bl_key.keys:
+        kitsu_key = md_descriptors_by_bl_key[bl_key].get('kitsu_key')
     return data
 
 def get_baselight_scene_shots(config, blpath):
