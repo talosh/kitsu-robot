ce36b9a94f6af92d281278e2d821e787d1f6b510
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-08-02 10:40:54 +0100

    update metadata_descriptors.json and sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -61,11 +61,15 @@ def link_baselight_sequence(config, baselight_linked_sequence):
         log.info('host "%s" is not defined in flapi_hosts config file' % blpath_components[0])
         return
     baselight_shots = get_baselight_scene_shots(config, blpath)
-    pprint (baselight_shots)
-    sys.exit()
+    # pprint (baselight_shots)
+    # sys.exit()
 
     project_dict = gazu.project.get_project(baselight_linked_sequence.get('project_id'))
     shots = gazu.shot.all_shots_for_sequence(baselight_linked_sequence)
+
+    metadata_descriptors = config.get('metadata_descriptors')
+    pprint (metadata_descriptors)
+    sys.exit()
 
     for baselight_shot in baselight_shots:
         shot_md = baselight_shot.get('shot_md')
