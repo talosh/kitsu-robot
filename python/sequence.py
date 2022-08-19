e91008ac8d546bd947137e5d8805a5fc04e882e1
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-08-19 16:42:43 +0100

    update sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -56,7 +56,6 @@ def link_baselight_sequence(config, gazu, baselight_linked_sequence):
     kitsu_uid_metadata_obj = check_or_add_kitsu_metadata_definition(config, blpath)
     if not kitsu_uid_metadata_obj:
         return
-    pprint (kitsu_uid_metadata_obj)
     baselight_shots = get_baselight_scene_shots(config, blpath)
     
     project_dict = gazu.project.get_project(baselight_linked_sequence.get('project_id'))
@@ -69,7 +68,8 @@ def link_baselight_sequence(config, gazu, baselight_linked_sequence):
         shot_md = baselight_shot.get('shot_md')
         if not shot_md:
             continue
-        pprint (shot_md)
+        kitsu_uid = shot_md.get(kitsu_uid_metadata_obj.Key)
+        pprint (kitsu_uid)
         continue
         shot_name = create_kitsu_shot_name(config, baselight_shot)
         shot_data = build_kitsu_shot_data(config, baselight_shot)
