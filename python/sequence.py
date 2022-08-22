5638e486923a5b306bbc58f3791fd668c484b088
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-08-22 16:49:16 +0100

    update sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -110,7 +110,7 @@ def link_baselight_sequence(config, gazu, baselight_linked_sequence):
         exSettings.Overwrite = flapi.EXPORT_OVERWRITE_REPLACE
         exSettings.Directory = '/var/tmp'
         exSettings.Frames = flapi.EXPORT_FRAMES_FIRST 
-        exSettings.Filename = shot_id
+        exSettings.Filename = "%{Job}/%{Clip}_%{TimelineFrame}"
         exSettings.Source = flapi.EXPORT_SOURCE_SELECTEDSHOTS
 
         log.verbose( "Submitting to queue" )
