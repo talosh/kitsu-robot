bddd20d21d88c5aed3c08991915a727ea07fef39
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-08-23 16:25:58 +0100

    update sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -141,8 +141,11 @@ def link_baselight_sequence(config, gazu, baselight_linked_sequence):
                 thumbnail_file_name
             )
             thumbnail_local_path = config.get('temp_folder', '/var/tmp')
+            if not thumbnail_local_path.endswith(os.path.sep):
+                thumbnail_local_path = thumbnail_local_path + os.path.sep
             pprint ((thumbnail_remote_path, thumbnail_local_path))
-
+        
+        sys.exit()
 
         new_shot = gazu.shot.new_shot(
             project_dict, 
