2d8c41d84d97ede6a2d78e3c0e26b73550364226
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-08-30 18:20:09 +0100

    update sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -102,6 +102,7 @@ def sync_shot_marks(config, gazu, baselight_linked_sequence):
             continue
         shot = scene.get_shot(baselight_shot['shot_id'])
         src_start_frame = shot.get_src_start_frame()
+        pprint (src_start_frame)
         mark_ids = shot.get_mark_ids()
         if len(mark_ids) > 0:
             for ix,m in enumerate(mark_ids):
