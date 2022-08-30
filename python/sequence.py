54312f08bccfe9ff50c9ab613f259181ca5f57c0
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-08-30 15:27:38 +0100

    update sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -50,7 +50,8 @@ def sync_shot_marks(config, gazu, baselight_linked_sequence):
         data = kitsu_shot.get('data')
         if not data:
             continue
-        pprint (kitsu_shot)
+        if data.get('00_shot_id') == 'TFM_VFX_101_010'
+            pprint (kitsu_shot)
         locator = data.get('01-locator')
         if not locator:
             continue
