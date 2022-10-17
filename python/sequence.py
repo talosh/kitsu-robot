8f26b0f8e95169d5cddbe2c728b7c97bb2fce9db
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-10-17 11:09:07 +0100

    update sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -143,7 +143,7 @@ def sync_filenames_and_version_numbers(config, gazu, baselight_linked_sequence):
 def sync_shot_marks(config, gazu, baselight_linked_sequence):
     log = config.get('log')
     print ('\n')
-    log.verbose('--- Syncing shot marks ---\n')
+    log.verbose('--- Syncing shot marks ---')
 
     def parse_locator(localtor_string, mark_categories):
         import json
