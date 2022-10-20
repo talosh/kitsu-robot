eeac117abcd5241995c86545de6ae7611c753712
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-10-20 12:10:26 +0100

    update backup_zou_db.sh

--- robotenv/bin/backup_zou_db.sh
+++ robotenv/bin/backup_zou_db.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 /usr/bin/osascript -e 'tell application "Terminal" to activate'
 /usr/bin/osascript -e 'tell application "Terminal" to do script "/bin/bash -rcfile /opt/kitsu-robot/robotenv/bin/backup_zou_db.rc" in window 1'
-sleep 2
-/usr/bin/osascript -e 'quit application "Terminal"'
+# sleep 2
+# /usr/bin/osascript -e 'quit application "Terminal"'
