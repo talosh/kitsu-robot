8cff0762a0d51fa9d621d191b6a6679e9d1c534f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-10-20 12:04:53 +0100

    update backup_zou_db.sh

--- robotenv/bin/backup_zou_db.sh
+++ robotenv/bin/backup_zou_db.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 /usr/bin/osascript -e 'tell application "Terminal" to activate'
-/usr/bin/osascript -e 'tell application "Terminal" to do script "/bin/bash -c "cd /Volumes/storage/opt/zou/backups/; DB_PASSWORD=mysecretpassword /opt/zou/zouenv/bin/zou dump-database"" in window 1'
+/usr/bin/osascript -e 'tell application "Terminal" to do script "/bin/bash -rcfile /opt/kitsu-robot/robotenv/bin/backup_zou_db.rc" in window 1'
 sleep 2
 /usr/bin/osascript -e 'quit application "Terminal"'
