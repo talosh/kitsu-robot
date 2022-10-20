35ebc3ef7c0747ade369a80ac39092f356b42753
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-10-20 15:51:17 +0100

    update sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -202,8 +202,11 @@ def sync_shot_marks(config, gazu, baselight_linked_sequence):
     blpath = baselight_linked_sequence.get('blpath')
     baselight_shots = baselight_linked_sequence.get('baselight_shots')
     kitsu_uid_metadata_obj = baselight_linked_sequence.get('kitsu_uid_metadata_obj')
-    kitsu_shots = baselight_linked_sequence.get('kitsu_shots')
-    baselight_shots_by_kitsu_uid = {x['shot_md'].get(kitsu_uid_metadata_obj.Key):x for x in baselight_shots}
+    if kitsu_uid_metadata_obj:
+        kitsu_shots = baselight_linked_sequence.get('kitsu_shots')
+        baselight_shots_by_kitsu_uid = {x['shot_md'].get(kitsu_uid_metadata_obj.Key):x for x in baselight_shots}
+    else:
+        return
 
     flapi = import_flapi(config)
     flapi_host = resolve_flapi_host(config, blpath)
