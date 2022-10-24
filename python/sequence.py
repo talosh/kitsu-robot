142ed488d30b04275ad3c5560915be68d5854ab9
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-10-24 16:55:41 +0100

    update sequence.py

--- python/sequence.py
+++ python/sequence.py
@@ -422,8 +422,6 @@ def populate_kitsu_from_baselight_sequence(config, gazu, baselight_linked_sequen
         shot_id = baselight_shot.get('shot_id')
         shot = scene.get_shot(shot_id)
 
-        '''
-
         try:
             qm = conn.QueueManager.create_local()
         except flapi.FLAPIException as ex:
@@ -512,8 +510,6 @@ def populate_kitsu_from_baselight_sequence(config, gazu, baselight_linked_sequen
             os.remove(thumbnail_local_path)
         except:
             pass
-
-        '''
 
         new_md_values = {
             kitsu_uid_metadata_obj.Key: new_shot.get('id')
