a1d0fe08f456563380bcf17961ab224666d0a76b
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2022-12-09 11:03:12 +0000

    update robot.py

--- robot.py
+++ robot.py
@@ -48,7 +48,9 @@ if __name__ == "__main__":
     for app_config_key in app_config.keys():
         app_data['config'][app_config_key] = app_config[app_config_key]
 
-
+    pprint (app_data['config'].copy())
+    sys.eixt()
+    
     '''
     weblog_thread = threading.Thread(target=tailon, args=(app_config, ))
     weblog_thread.daemon = True
