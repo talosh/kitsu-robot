037ad22956fd2d57045c23763e454186ca9efc7b
Andriy Toloshnyy <andriy.toloshnyy@gmail.com>
2022-12-10 15:59:50 +0000

    logging

--- python/baselight.py
+++ python/baselight.py
@@ -1,18 +1,20 @@
 import os
 import sys
 import time
+import threading
 
 from .util import remote_listdir
 from .util import remote_rm
 from .util import rsync
 
 from .config import get_config_data
+from .util import RobotLog
 
 from pprint import pprint, pformat
 
 
 def baselight_process(app_data):
-    log = app_data['config'].get('log')
+    log = RobotLog(app_data['config'], filename = 'baselight.log')
 
     while True:
         try:
