29fdbeaeeebdea0efb32356fdfde98aa32997cee
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-01-03 17:06:55 +0000

    update kitsu.py

--- python/kitsu.py
+++ python/kitsu.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import threading
 from .config import get_config_data
 from pprint import pprint, pformat
 
@@ -11,10 +12,16 @@ from .util import RobotLog
 def kitsu_loop(app_data):
     log = RobotLog(app_data['config'], filename = 'kitsu.log')
 
-    pprint (app_data['config'].copy())
-
     while True:
         try:
+            config = app_data.get('config').copy()
+            config_gazu = config.get('gazu')
+            host = config_gazu.get('host')
+            name = config_gazu.get('name')
+            password = config_gazu.get('password')
+
+            pprint (config_gazu)
+
             time.sleep(4)
         except KeyboardInterrupt:
             return
